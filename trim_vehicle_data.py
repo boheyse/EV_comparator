24656dc98ee8d57b4eac1e544bd8320d30380f47
Bo Heyse <Heyseb1@Bos-MacBook-Pro.local>
2019-05-26 20:26:07 -0500

    automated way to pull needed data from .gov csv files

---
+++ trim_vehicle_data.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+import types
+
+file_path = "/Users/Heyseb1/Downloads/vehicles.csv"
+data = pd.read_csv(file_path)
+
+num_vehicles = len(data.index)
+print(num_vehicles)
+
+gallons = data.iloc[0:num_vehicles, 0]
+city_mpg = data.iloc[0:num_vehicles, 4]
+avg_mpg = data.iloc[0:num_vehicles, 15]
+highway_mpg = data.iloc[0:num_vehicles, 34]
+make = data.iloc[0:num_vehicles, 46]
+model = data.iloc[0:num_vehicles, 47]
+year = data.iloc[0:num_vehicles, 63]
+
+
+Data = {'gallons':  gallons,
+        'city_mpg': city_mpg,
+        'avg_mpg': avg_mpg,
+        'highway_mpg': highway_mpg,
+        'make': make,
+        'model': model,
+        'year': year
+        }
+
+df = pd.DataFrame(Data, columns = ['gallons','city_mpg', 'avg_mpg', 'highway_mpg', 'make', 'model', 'year'])
+df.to_csv("June2019Vehicles.csv", index=False)
